9c769d76ca1481748133697939f58c41a9dba1bc
jmjamison <jamie@jmjamison.com>
2015-10-26 18:29:43 -0700

    151026jmj: new menubar (uses adobe SpryAssests)

---
+++ _includes/SSDA_menubar.php
@@ -0,0 +1,46 @@
+<!-- The structure of this file is exactly the same as 2col_rightNav.html;
+
+     the only difference between the two is the stylesheet they use -->
+
+<script src="../index/SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+
+<link href="../index/SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css">
+
+
+<ul id="MenuBar2" class="MenuBarHorizontal">
+<li><a href="https://www.library.ucla.edu/location/social-science-data-archive">Data Archive at UCLA Library</a>  </li>
+<li><a href="#" class="MenuBarItemSubmenu">Archive Data Catalog: Search by</a>
+<!-- -->
+<ul><li> <a href="http://data-archive.library.ucla.edu/da_catalog/da_catalog_index.php" title="Search by index terms">Index Terms</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog/da_catalog_titles.php" title="Search by study titles">Study Titles</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog/da_catalog_studynumbers.php" title="Search by study titles">Study Numbers</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog/da_catalog_search.php" title="Search Database">Keyword or Term</a></li>
+     <li> <a href="http://data-archive.library.ucla.edu/da_catalog/index.php" title="About the Collection">About the Collection</a></li></ul>
+  </li>
+<!-- -->
+ <li><a href="#" class="MenuBarItemSubmenu">Eve Fielder Collection</a>
+<ul><li> <a href="http://data-archive.library.ucla.edu/da_catalog_fielder/fielder_index.php" title="Search by index terms">Index Terms</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog_fielder/fielder_titles.php" title="Search by study titles">Study Titles</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog_fielder/fielder_authors.php" title="Search by study titles">Authors</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog_fielder/fielder_search.php" title="Search Database">Keyword or Term</a></li>
+    <li> <a href="http://data-archive.library.ucla.edu/da_catalog_fielder/index.php" title="About the Collection">About the Collection</a></li></ul>
+  </li>
+<!-- -->
+<li><a href="#" class="MenuBarItemSubmenu">Archive Resources</a>
+<ul><li><a href="http://data-archive.library.ucla.edu/archive resources/portalsresources.php">Data Portals</a></li>
+<li><a href="http://data-archive.library.ucla.edu/archive resources/toolsresources.php">Data Analysis Tools</a></li>
+<li><a href="http://data-archive.library.ucla.edu/archive resources/indexresources.php">Indexes</a></li>
+<li><a href="http://data-archive.library.ucla.edu/archive resources/surveyresearchresources.php">Survey Research Resources</a></li>
+<li><a href="http://data-archive.library.ucla.edu/archive resources/instructionalsupportresources.php">Instruction Resources</a></li>
+<li><a href="http://data-archive.library.ucla.edu/archive resources/qualitativedataresources.php">Qualitative Data Analysis</a></li></ul>
+    </li>
+<li><a href="http://www.icpsr.umich.edu/icpsrweb/ICPSR/">ICPSR</a></li>
+<li><a href="http://www.ucla.edu/">UCLA</a></li></ul>
+
+<script type="text/javascript">var MenuBar2 = new Spry.Widget.MenuBar("MenuBar2", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});</script>
+
+
+
+
+
+
