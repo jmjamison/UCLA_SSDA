c5eb737c904d849062fc6459bd92270b65a63ed0
jmjamison <jamie@jmjamison.com>
2016-06-15 12:17:22 -0700

    20160615jmj: Corrected url paths for drupal pages.  (Example: /location/social-science-data-archive chanted to /social-science-data-archive). Change the links on the include pages to reflect this.

---
+++ _includes/SSDA_LibraryBottomPanel.php
@@ -0,0 +1,112 @@
+<!---------------------------------------------------------------------------------------------- -->
+
+
+
+</div></div></div>  </div>
+
+  
+  </div>
+  
+  
+</div>  </div>
+
+  
+  </div>
+</div>
+<div class="ui-tabs-panel ui-widget-content ui-corner-bottom ui-tabs-hide" id="tabs-0-below_main_left-2"><div class="panel-pane pane-bean-upcoming-events pane-bean-social-science-data-archive">
+<div class="pane-content">
+    <div class="entity entity-bean bean-upcoming-events clearfix">
+
+  <div class="content">
+      </div>
+</div>
+  </div>
+
+  
+  </div>
+</div></div>      </div>
+  </div>
+</div>
+
+
+  
+  
+      </div>
+
+                </div>
+  </div>
+
+  <div class="l-colophon-wrapper">
+    <div class="l-colophon" role="contentinfo">
+        <div class="l-region l-region--colophon-first">
+    <div class="panel-pane pane-bean-text-block pane-bean-colophon-about">
+  
+      
+  
+  <div class="pane-content">
+    <div class="entity entity-bean bean-text-block clearfix">
+
+  <div class="content">
+    <div class="field field--name-field-text-block field--type-text-long field--label-hidden"><div class="field__items">
+    
+ <div class="field__item even">
+<img src="../_images/logo-colophon.png" alt="UCLA Library Logo" width="150" height="30">
+
+<p>The mission of the UCLA Library is to provide access to and delivery 
+of information resources to UCLA students, faculty, and staff in support
+ of the research and instructional mission of the university. Relying on
+ its highly skilled staff, the Library encourages innovation, 
+capitalizes on appropriate technologies, forges effective partnerships 
+and aggressively promotes excellence.</p>
+</div></div></div>  </div>
+</div> </div>  
+  </div>
+  </div>
+        <div class="l-region l-region--colophon-second">
+    <div class="panel-pane pane-block pane-menu-menu-colophon-first">
+  
+      
+  
+  <div class="pane-content">
+    <ul class="menu"><li class="first leaf"><a href="https://www.library.ucla.edu/about" title="">About UCLA Library</a></li>
+<li class="leaf"><a href="https://www.library.ucla.edu/about/about-collections" title="">About the Collections</a></li>
+<li class="last leaf"><a href="https://www.library.ucla.edu/about/giving-library" title="">Giving to UCLA Library</a></li>
+</ul>  </div>
+
+  
+  </div>
+  </div>
+        <div class="l-region l-region--colophon-third">
+    <div class="panel-pane pane-block pane-menu-menu-colophon-second">
+  
+      
+  
+  <div class="pane-content">
+    <ul class="menu"><li class="first leaf"><a href="https://www.library.ucla.edu/about/employment-human-resources" title="">Employment</a></li>
+<li class="leaf"><a href="http://www.ucla.edu/" title="">UCLA Homepage</a></li>
+<li class="last leaf"><a href="https://www.library.ucla.edu/about/social-media-directory" title="">Social Media Directory</a></li>
+</ul>  </div>
+
+  
+  </div>
+  </div>
+    </div>
+  </div>
+
+  <div class="l-footer-wrapper">
+    <div class="l-footer" role="contentinfo">
+        <div class="l-region l-region--footer">
+    <div class="panel-pane pane-block pane-menu-menu-footer">
+  
+      
+  
+  <div class="pane-content">
+    <ul class="menu"><li class="first last leaf"><span title="" class="footer-copyright nolink">2015 © UC Regents</span></li>
+</ul>  </div>
+
+  
+  </div>
+  </div>
+    </div>
+  </div>
+</div>
---
+++ _includes/SSDA_LibrarySidePanel.php
@@ -0,0 +1,19 @@
+<div class="pane-content">
+    <div class="menu-block-wrapper menu-block-1 menu-name-main-menu parent-mlid-0 menu-level-3">
+  <ul class="menu"><li class="first leaf menu-mlid-3519"><a href="https://www.library.ucla.edu/social-science-data-archive/about-data-archive">About the Data Archive</a></li>
+<li class="leaf menu-mlid-3520"><a href="https://www.library.ucla.edu/social-science-data-archive/data-archive-catalog">Archive Catalog</a></li>
+<li class="leaf menu-mlid-3520"><a href="https://www.library.ucla.edu/social-science-data-archive/archive-collection-archiving-policy">Data Archive Collection and Archiving Policy</a></li>
+<li class="last leaf menu-mlid-3521"><a href="https://www.library.ucla.edu/social-science-data-archive/archive-tutorials">Archive Tutorials and Resources</a></li>
+</ul></div>
+  </div>
+ </div>
+<div class="panel-pane pane-quick-links sidebar-tier-2">
+  
+        <h2 class="pane-title">Quick Links</h2>
+    
+  
+  <div class="pane-content">
+    <div class="field field--name-field-quick-links field--type-link-field field--label-hidden"><div class="field__items"><div class="field__item even"><a href="http://www.icpsr.umich.edu/icpsrweb/ICPSR/" target="_blank">ICPSR</a></div><div class="field__item odd"><a href="http://www.ropercenter.uconn.edu/" target="_blank">Roper Center</a></div><div class="field__item even"><a href="https://www.library.ucla.edu/social-science-data-archive/data-related-videos">Data Related Videos</a></div><div class="field__item odd"><a href="https://www.library.ucla.edu/social-science-data-archive/data-portals">Data Portals</a></div></div></div>  </div>
+
+  
+  </div>
